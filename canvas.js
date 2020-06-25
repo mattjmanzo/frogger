d2f6e8cf0254e3d6a2b63f76caf506672fae0fd3
Matthew Manzo <matthew.manzo1@gmail.com>
2020-06-25 14:42:15 -0400

    final update

--- canvas.js
+++ canvas.js
@@ -43,13 +43,20 @@ const frog = {
   height: 150,
 };
 
-const carImage = new Image();
-carImage.src = "car.png";
-// carImage.onload = animate;
+// Modify details to drawCar1
+// const carImage1 = new Image();
+// carImage1.src = "car1.png";
 function drawCar(car) {
   car.x += car.speed;
-  ctx.drawImage(carImage, car.x, car.y, car.width, car.height);
+  ctx.drawImage(car.carImage, car.x, car.y, car.width, car.height);
 }
+
+// const carImage2 = new Image();
+// carImage2.src = "car2.png";
+// function drawCar2(car) {
+//   car.x -= car.speed;
+//   ctx.drawImage(carImage2, car.x, car.y, car.width, car.height);
+// }
 
 function drawCars() {
   cars.forEach((car) => drawCar(car));
@@ -58,16 +65,48 @@ function drawCars() {
 const cars = [];
 // creates a new car every 1000 milliseconds
 setInterval(function () {
+  const carImage1 = new Image();
+  carImage1.src = "car1.png";
   let start = canvas.height / 9;
   let laneHeight = canvas.height / 1.4 / 4;
+  let first = Math.random() < 0.5;
+  let lane;
+  if (first) {
+    lane = start + laneHeight * 0 + laneHeight / 2 - 25;
+  } else {
+    lane = start + laneHeight * 2 + laneHeight / 2 - 25;
+  }
   cars.push({
+    carImage: carImage1,
     x: 0,
-    y: start + laneHeight * Math.floor(Math.random() * 4) + laneHeight / 2 - 25,
+    y: lane, // y: start + laneHeight * Math.floor(Math.random() * 4) + laneHeight / 2 - 25,
     width: 50,
     height: 50,
     speed: 1 + 1.5 * level,
   });
-}, 1000);
+}, 2000);
+
+setInterval(function () {
+  const carImage2 = new Image();
+  carImage2.src = "car2.png";
+  let start = canvas.height / 9;
+  let laneHeight = canvas.height / 1.4 / 4;
+  let first = Math.random() < 0.5;
+  let lane;
+  if (first) {
+    lane = start + laneHeight * 1 + laneHeight / 2 - 25;
+  } else {
+    lane = start + laneHeight * 3 + laneHeight / 2 - 25;
+  }
+  cars.push({
+    carImage: carImage2,
+    x: canvas.width + 100,
+    y: lane, // y: start + laneHeight * Math.floor(Math.random() * 4) + laneHeight / 2 - 25,
+    width: 50,
+    height: 50,
+    speed: (1 + 1.5 * level) * -1,
+  });
+}, 2000);
 
 function drawHighway() {
   ctx.fillStyle = "#3a4049";
@@ -97,7 +136,7 @@ function drawLanes() {
 
 function drawGrass() {
   ctx.fillStyle = "#50C878";
-  ctx.fillRect(0, 580, canvas.width, canvas.height / 4);
+  ctx.fillRect(0, 560, canvas.width, canvas.height / 4);
 }
 
 document.body.onkeydown = function (event) {
@@ -125,9 +164,9 @@ document.body.onkeydown = function (event) {
 function collisionDetection() {
   cars.forEach((car) => {
     if (
-      frog.x <= car.x + car.width - 35 && // collision of frog if hit on left side - this is good
-      frog.x + frog.width >= car.x + 50 && // collision of frog if hit on right side - this is good
-      frog.y <= car.y + car.height - 60 && // collision of frog if hit on top side - this is good
+      frog.x <= car.x + car.width - 35 && // collision of frog if hit on left side
+      frog.x + frog.width >= car.x + 50 && // collision of frog if hit on right side
+      frog.y <= car.y + car.height - 60 && // collision of frog if hit on top side
       frog.y + frog.height >= car.y - 30 // collision of frog if hit on bottom side
     ) {
       frog.x = canvas.width / 2 - 75;
